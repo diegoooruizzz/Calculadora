02330c9c33f723252783cd500d1bedb057c58025
AzureAD\GuillemRodriguezVizc <guillem.rodriguez@estudiant.enti.cat>
2025-11-20 09:55:14 +0100

    correction

--- Calculadora/Calculadora/Source.cpp
+++ Calculadora/Calculadora/Source.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
+
+float suma(float a, float b) {
+    return a + b;
+}
+
+float resta(float a, float b) {
+    return a - b;
+}
+
+float multiplicacio(float a, float b) {
+    return a * b;
+}
+
+float divisio(float a, float b) {
+    if (b != 0)
+        return (a / b);
+    else {
+        cout << "no es pot dividir entre 0" << endl;
+        return 0;
+    }
+}
+
+
 int main() {
 
-	std::cout << "Hola Mundo";
-	std::cout << "Hola";
 }
 
 
